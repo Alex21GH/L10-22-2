fc3d942beda220f01ba2d94bd2eebf4e837138e1
Alex21GH <89952595+Alex21GH@users.noreply.github.com>
2023-11-08 16:00:54 -0500

    Carga inicial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexis
+ *
+ * Created on 8 de noviembre de 2023, 03:57 PM
+ */
+
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
